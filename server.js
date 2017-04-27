bbf20d8cf31e0d0c6c1203d51d6a70c15bec3b97
levinsonmm <levinsonmm@gmail.com>
2017-04-27 00:19:29 -0400

    added some partials and stuff

--- server.js
+++ server.js
@@ -4,24 +4,62 @@ var port = process.env.PORT || 8080
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-var articles = {}
-createArticle({
-  title: 'Squirrel escapes squirrel prison, now at large!',
-  content: 'Yesterday evening at 7:47PM, the criminal squirrel Rabid Rabindra got out of his cage.'
+var objs = {}
+
+createObject('Home', {
+  'title': 'Home',
+  'link': '/',
+  'image': 'https://i.scdn.co/image/0c1636f797ad833fc4f37046fb391b03f43bc196',
+  alt: ''
 })
-createArticle({
-  title: 'Squirrel lured back into prison with peanut butter',
-  content: 'This morning at 10:13PM, the smell of peanut butter drew Rabid Rabindra the squirrel back into his cage.'
+
+createObject('Band1', {
+  title: 'Band1',
+  link: '/band1',
+  image: 'https://f4.bcbits.com/img/0009206218_10.jpg',
+  alt: ''
 })
-function createArticle (article) {
-  var id = Object.keys(articles).length
-  article.createdAt = new Date()
-  articles[id] = article
+
+createObject('Band2', {
+  title: 'Band2',
+  link: '/band2',
+  image: 'https://upload.wikimedia.org/wikipedia/en/6/6e/Thedevilandgodareraginginsideme.jpg',
+  alt: ''
+})
+
+createObject('Band3', {
+  title: 'Band3',
+  link: '/band3',
+  image: 'https://upload.wikimedia.org/wikipedia/en/b/b7/Joyce_Manor_Never_Hungover_Again.png',
+  alt: ''
+})
+
+function createObject (id, obj) {
+  objs[id] = obj
 }
 
 app.get('/', function (request, response) {
   response.render('pages/index', {
-    articles: articles
+    objs: objs,
+    obj: objs['Home']
+  })
+})
+app.get('/band1', function (request, response) {
+  response.render('pages/band', {
+    objs: objs,
+    obj: objs['Band1']
+  })
+})
+app.get('/band2', function (request, response) {
+  response.render('pages/band', {
+    objs: objs,
+    obj: objs['Band2']
+  })
+})
+app.get('/band3', function (request, response) {
+  response.render('pages/band', {
+    objs: objs,
+    obj: objs['Band3']
   })
 })
 
